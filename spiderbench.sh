3c5aeecf61606a1e660d57cb2f428b7610101b41
John Richardson <johnmaster208@gmail.com>
2017-03-08 10:03:18 -0600

    Wrapping main() function to spiderbench.sh

--- spiderbench.sh
+++ spiderbench.sh
@@ -26,7 +26,7 @@ spinner() {
 }
 crawl() {
 	touch 'crawl.txt'
-	wget --spider -r -nv -m -np -R js,css,jpeg,jpg,png,pdf --reject-regex '(.*)\?(.*)' -o crawl.txt $URL
+	wget --spider -r -nv -m -np -R js,css,jpeg,jpg,png,pdf --reject-regex '(.*)\?(.*)' -o crawl.txt --no-check-certificate $URL
 }
 genlinks() {
 	touch 'links.txt'
@@ -35,85 +35,87 @@ genlinks() {
 	# so we can use the unix 'read' command, add a blank line at end of file.
 	sed -i '' -e '$a\' links.txt
 }
-
-
-clear
-printf "${DEFAULT}"
-echo "Checking dependencies..."
-sleep 1
-echo "Looking for Homebrew installation..."
-sleep $[ ( $RANDOM % 5 )  + 1 ]s
-if [[ -z "$HB" ]]; then
-	echo "${RED}We didn't find Homebrew installed on your system.${DEFAULT}"
-else
-	echo "Homebrew is installed on your system ${BLUE}[OK]${DEFAULT}."
-fi
-echo "Looking for wget installation..."
-sleep $[ ( $RANDOM % 5 )  + 1 ]s
-if [[ -z "$WGET" ]]; then
-	echo "${RED}We didn't find wget installed on your system.${DEFAULT}"
-	exit
-else
-	echo "wget utility is installed on your system ${BLUE}[OK]${DEFAULT}"
-fi
-echo "Looking for Apache Bench installation..."
-sleep $[ ( $RANDOM % 5 )  + 1 ]s
-if [[ -z "$AB" ]]; then
-	echo "${RED}We didn't find Apache Bench (ab) installed on your system.${DEFAULT}"
-	read -p "Would you like to install it now? (Y/n): " ab
-		case $ab in
-			[Yy]*) INSTALL_AB=1; echo "Installing Apache Bench...";;
-			[Nn]*) echo "Ok, we won't install right now. Goodbye!"; exit;; 
-		esac
-	if [ $INSTALL_AB -eq 1 ] && [ -z "$HB" ]; then
-		sudo apt-get install apache2utils
-	elif [[ $INSTALL_AB -eq 1 ]]; then
-		brew install ab
+main() {
+	clear
+	printf "${DEFAULT}"
+	echo "Checking dependencies..."
+	sleep 1
+	echo "Looking for Homebrew installation..."
+	sleep $[ ( $RANDOM % 5 )  + 1 ]s
+	if [[ -z "$HB" ]]; then
+		echo "${RED}We didn't find Homebrew installed on your system.${DEFAULT}"
+	else
+		echo "Homebrew is installed on your system ${BLUE}[OK]${DEFAULT}."
 	fi
-	
-else
-	echo "Apache Bench was installed on your system ${BLUE}[OK]${DEFAULT}."
-fi
-sleep 1
-echo "Cleaning up some older files..."
-if [[ -f ./crawl.txt ]]; then
-	echo "Previous 'crawl.txt' found. Removing."
-	rm ./crawl.txt
-fi
-if [[ -f ./links.txt ]]; then
-	echo "Previous 'links.txt' file was found. Removing."
-	rm ./links.txt
-fi
-if [[ -f ./output.csv ]]; then
-	echo "Previous 'output.csv' file was found. Removing."
-	rm ./output.csv
-fi
-echo "System check COMPLETED!"
-echo "Press the ENTER key to get started..."
-read -n 1 -s
-sleep 1
-clear
-read -p "Enter the URL of the website you'd like to benchmark [http://mydomain.com], without the trailing slash (/) on the end: " URL
-echo "URL saved as ${BLUE}$URL${DEFAULT}"
-sleep 1
-echo "Next, we'll create a sitemap with ${BLUE}wget${DEFAULT}"
-echo "Crawling $URL..."
-crawl & spinner $!
-echo "Crawl ${BLUE}FINISHED!${DEFAULT}"
-sleep 1
+	echo "Looking for wget installation..."
+	sleep $[ ( $RANDOM % 5 )  + 1 ]s
+	if [[ -z "$WGET" ]]; then
+		echo "${RED}We didn't find wget installed on your system.${DEFAULT}"
+		exit
+	else
+		echo "wget utility is installed on your system ${BLUE}[OK]${DEFAULT}"
+	fi
+	echo "Looking for Apache Bench installation..."
+	sleep $[ ( $RANDOM % 5 )  + 1 ]s
+	if [[ -z "$AB" ]]; then
+		echo "${RED}We didn't find Apache Bench (ab) installed on your system.${DEFAULT}"
+		read -p "Would you like to install it now? (Y/n): " ab
+			case $ab in
+				[Yy]*) INSTALL_AB=1; echo "Installing Apache Bench...";;
+				[Nn]*) echo "Ok, we won't install right now. Goodbye!"; exit;; 
+			esac
+		if [ $INSTALL_AB -eq 1 ] && [ -z "$HB" ]; then
+			sudo apt-get install apache2utils
+		elif [[ $INSTALL_AB -eq 1 ]]; then
+			brew install ab
+		fi
+		
+	else
+		echo "Apache Bench was installed on your system ${BLUE}[OK]${DEFAULT}."
+	fi
+	sleep 1
+	echo "Cleaning up some older files..."
+	if [[ -f ./crawl.txt ]]; then
+		echo "Previous 'crawl.txt' found. Removing."
+		rm ./crawl.txt
+	fi
+	if [[ -f ./links.txt ]]; then
+		echo "Previous 'links.txt' file was found. Removing."
+		rm ./links.txt
+	fi
+	if [[ -f ./output.csv ]]; then
+		echo "Previous 'output.csv' file was found. Removing."
+		rm ./output.csv
+	fi
+	echo "System check COMPLETED!"
+	echo "Press the ENTER key to get started..."
+	read -n 1 -s
+	sleep 1
+	clear
+	read -p "Enter the URL of the website you'd like to benchmark [http://mydomain.com], without the trailing slash (/) on the end: " URL
+	echo "URL saved as ${BLUE}$URL${DEFAULT}"
+	sleep 1
+	echo "Next, we'll create a sitemap with ${BLUE}wget${DEFAULT}"
+	echo "Crawling $URL..."
+	crawl & spinner $!
+	echo "Crawl ${BLUE}FINISHED!${DEFAULT}"
+	sleep 1
 
-echo "Building Links list..."
-genlinks & spinner $!
-echo "Links generated SUCCESSFULLY. Open the ${BLUE}links.txt${DEFAULT} file to see them."
-echo "Press the ENTER key to run the Apache Bench tests.."
-read -n 1 -s
-clear
+	echo "Building Links list..."
+	genlinks & spinner $!
+	echo "Links generated SUCCESSFULLY. Open the ${BLUE}links.txt${DEFAULT} file to see them."
+	echo "Press the ENTER key to run the Apache Bench tests.."
+	read -n 1 -s
+	clear
 
-echo "Running Bench tests..."
-touch output.csv
-echo "URL,MIN,MEAN,SD,MED,MAX" >> output.csv
-while read url; do
-echo "$url, $(ab -n 5 -c 5 $URL/ | sed -n '/Total:/p' | awk -v OFS=, '{ print $2, $3, $4, $5, $6 }')" >> output.csv 
-done < links.txt & spinner $!
+	echo "Running Bench tests..."
+	touch output.csv
+	echo "URL,MIN,MEAN,SD,MED,MAX" >> output.csv
+	while read url; do
+	echo "$url, $(ab -n 5 -c 5 $URL/ | sed -n '/Total:/p' | awk -v OFS=, '{ print $2, $3, $4, $5, $6 }')" >> output.csv 
+	done < links.txt & spinner $!
 
-echo "Bench tests COMPLETED. Look at ${BLUE}output.csv${DEFAULT} for your bench test output!"
+	echo "Bench tests COMPLETED. Look at ${BLUE}output.csv${DEFAULT} for your bench test output!"
+}
+
+main
